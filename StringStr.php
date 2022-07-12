0f8e9a302d1b40f3f4c446b2dcc7a150c71c9a1b
Shreyash Shenoy <shenoyshreyash95@gmail.com>
2022-07-12 21:51:34 +0530

    removing leetcode problem for July

--- StringStr.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$haystack = "hello"; $needle = "ll";
-
-function strStr($haystack, $needle) {
-    $strPos  = strpos($haystack, $needle);
-      return $strPos !== false ? $strPos : -1;
-
-  }
