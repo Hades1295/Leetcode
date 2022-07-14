bba854d03b00502014d878e772fdaba590d26edb
Shreyash Shenoy <shenoyshreyash95@gmail.com>
2022-07-14 23:01:43 +0530

    adding Leet code for Search Insert Position

--- July/searchInsertPosition.php
+++ July/searchInsertPosition.php
@@ -1,11 +1,15 @@
 <?php
 
-$nums = [1,3,5,6]; $target = 1;
+$nums = [1,3,5,6]; $target = 7;
     print_r(searchInsert($target, $nums));
-    function searchInsert($target, $nums) {
-        if (array_search($target,$nums)) {
-            return array_search($target,$nums);
-        }else {
-            return false;
+    function searchInsert($nums, $target) {
+        foreach ($nums as $key => $value) {
+            if ($value >= $target) {
+                return $key;
+                break;
+            }
+            if (end($nums)< $target) {
+                return end($key);
+            }
         }
-    }
+      }
