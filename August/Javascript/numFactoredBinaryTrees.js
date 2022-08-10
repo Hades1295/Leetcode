08d2d6194935cc9b7a21e704ffff80ff20613542
Shreyash Shenoy <shenoyshreyash95@gmail.com>
2022-08-10 23:33:20 +0530

    adding leet code to factor binary tree

---
+++ August/Javascript/numFactoredBinaryTrees.js
@@ -0,0 +1,35 @@
+// <!-- Given an array of unique integers, arr, where each integer arr[i] is strictly greater than 1.
+
+// We make a binary tree using these integers, and each number may be used for any number of times. Each non-leaf node's value should be equal to the product of the values of its children.
+
+// Return the number of binary trees we can make. The answer may be too large so return the answer modulo 109 + 7.
+
+ 
+
+// Example 1:
+
+// Input: arr = [2,4]
+// Output: 3
+// Explanation: We can make these trees: [2], [4], [4, 2, 2]
+// Example 2:
+
+// Input: arr = [2,4,5,10]
+// Output: 7
+// Explanation: We can make these trees: [2], [4], [5], [10], [4, 2, 2], [10, 2, 5], [10, 5, 2]. -->
+
+
+var numFactoredBinaryTrees = function(A) {
+    A.sort((a,b) => a - b)
+    let len = A.length, fmap = new Map(), ans = 0
+    for (let i = 0; i < len; i++) {
+        let num = A[i], ways = 1, lim = Math.sqrt(num)
+    
+        for (let j = 0, fA = A[0]; fA <= lim; fA = A[++j]) {
+            let fB = num / fA
+            if (fmap.has(fB))
+                ways += fmap.get(fA) * fmap.get(fB) * (fA === fB ? 1 : 2)
+        }
+        fmap.set(num, ways), ans += ways
+    }
+    return ans % 1000000007
+};
