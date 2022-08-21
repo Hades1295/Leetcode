5d4a9d772966477568fa820af03cff0608fa8fad
Shreyash Shenoy <shenoyshreyash95@gmail.com>
2022-08-21 18:50:00 +0530

    adding leet code to build an Array in PHP

---
+++ August/buildArray.php
@@ -0,0 +1,35 @@
+<?php
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+// Input: nums = [0,2,1,5,3,4]
+// Output: [0,1,2,4,5,3]
+// Explanation: The array ans is built as follows: 
+// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+//     = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+//     = [0,1,2,4,5,3]
+
+
+// Example 2:
+
+// Input: nums = [5,0,1,2,3,4]
+// Output: [4,5,0,1,2,3]
+// Explanation: The array ans is built as follows:
+// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+//     = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
+//     = [4,5,0,1,2,3]
+$nums = [0,2,1,5,3,4];
+print_r(buildArray($nums));
+function buildArray($nums) {
+    $temp= [];
+    //$temp =  $nums;
+    $size = sizeof($nums);
+    for ($i=0; $i < $size; $i++) { 
+      $temp[] = $nums[$i]; 
+    }
+    for ($j=0; $j < $size ; $j++) { 
+        $res[] = $nums[$temp[$j]]; 
+    }
+        return $res;
+}
