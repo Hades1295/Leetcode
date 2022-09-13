0e529381b3b5b709448fc240b351103f9d37ccd2
Shreyash Shenoy <shenoyshreyash95@gmail.com>
2022-09-13 23:34:34 +0530

    Adding leet code to interpret strings in php

---
+++ September/interpret.php
@@ -0,0 +1,28 @@
+<?php
+
+// You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+// Given the string command, return the Goal Parser's interpretation of command.
+
+// Input: command = "G()(al)"
+// Output: "Goal"
+// Explanation: The Goal Parser interprets the command as follows:
+// G -> G
+// () -> o
+// (al) -> al
+// The final concatenated result is "Goal".
+// Example 2:
+
+// Input: command = "G()()()()(al)"
+// Output: "Gooooal"
+// Example 3:
+
+// Input: command = "(al)G(al)()()G"
+// Output: "alGalooG"
+
+$command = "G()(al)";
+print_r(interpret($command));
+function interpret($command) {
+    return str_replace(['()', '(al)'], ['o', 'al'], $command);
+    
+}
